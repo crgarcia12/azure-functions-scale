17f0ed0cabc067976c0e0293bdc21278c891b2cb
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2023-07-24 13:08:35 +0200

    function init boilerplate

--- src/chunker/function_app.py
+++ src/chunker/function_app.py
@@ -1,13 +1,22 @@
+from azure.functions import QueueMessage
 import azure.functions as func
+
 import logging
 
 app = func.FunctionApp()
 
-
 @app.blob_trigger(arg_name="myblob", 
     path="raw",
-    connection="DefaultEndpointsProtocol=https;AccountName=crgarfuncscalerg;AccountKey=pbiy+AqmqRm9XZ53JYsXFrsNJpqW/Pw3G+xCYVu3ot/+D62clrUkkFSAOd5tWbn3MFhKhr17DL9Y+ASt3vHEwg==;EndpointSuffix=core.windows.net") 
+    connection="StorageAccountConnectionString") 
 def Chunk(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
+
+
+
+@app.queue_trigger(arg_name="azqueue", queue_name="blobchangequeue",
+                               connection="StorageAccountConnectionString") 
+def blobchange(azqueue: func.QueueMessage):
+    logging.info('Python Queue trigger processed a message: %s',
+                azqueue.get_body().decode('utf-8'))
